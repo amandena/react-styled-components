e997442b0cf01532fa8a5d4827e812b26a104f9a
amandena <manda.m.johnson@gmail.com>
2022-01-19 21:37:43 -0500

    set up app with router and routes & their respective components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import GlobalStyle from './globalStyles'
+import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignupPage from './pages/SignupPage'
+import PricingPage from './pages/PricingPage'
 
 function App() {
   return (
-    <div className='App'>
-      React Styled Components
-    </div>
+    <Router>
+      <GlobalStyle />
+
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/signup' exact component={SignupPage} />
+        <Route path='/pricing' exact component={PricingPage} />
+      </Switch>
+    </Router>
   )
 }
 
